386d42d004a1c943d31dc13b26408cbe4ac40c30
Nockech <49099272+Nockech@users.noreply.github.com>
2019-04-07 19:36:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def check_queue(id):
 
 @Bot.event
 async def on_ready():
-    await Bot.change_presence(game = discord.Game(name = "/help"))
+    await Bot.change_presence(game = discord.Game(name = "/help", type = 2))
     print("No errors , great job")
     print("Bot is online")
 
