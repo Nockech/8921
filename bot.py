8474c67d6fd168542233cb932d78c3c542f0b729
Nockech <49099272+Nockech@users.noreply.github.com>
2019-04-26 19:13:54 +0300

    Gg

--- bot.py
+++ bot.py
@@ -103,7 +103,7 @@ async def helphere(ctx):
 async def info(ctx, user: discord.User):
     emb = discord.Embed(title= "{}".format(":information_source:"), color= 0x39d0d6  )
     if user.id == '399575084521488385':
-        emb.add_field(name = "\_\_This is my owner!\_\_" , value = "}}} ")
+        emb.add_field(name = "This is my owner!" , value = " ")
     ifbot = str("")
     if user.bot == True:
         ifbot = str("**BOT**")
