2b51b3f648fea2db87a2cb7e82b68d104fb1329d
Nockech <49099272+Nockech@users.noreply.github.com>
2019-07-25 15:18:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,5 +120,12 @@ async def clear(ctx, amount= 10):
     await ctx.channel.purge(limit = amount)
     await ctx.send(embed=cln)
 
+@Bot.command(pass_context = True)
+async def show(ctx):
+    Guild = ctx.message.guild.id
+    channel = ctx.message.author.voice.channel.id
+    show = discord.Embed(colour = 0x39d0d6, title="Click here to watch:", url = f'https://discordapp.com/channels/{Guild}/{channel}')
+    show.set_footer(text = f'{ctx.message.author.voice.channel.name}')
+    await ctx.send(embed = embed)
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token)) 
