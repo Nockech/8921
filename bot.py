a2735436e47e7a1aa86fe498581454882878d43a
Nockech <49099272+Nockech@users.noreply.github.com>
2020-09-29 21:13:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,13 +120,20 @@ async def say(ctx):
     except:
         await ctx.send("Error!Try to type text after `/say`")
 
+#INVITE
 @Bot.command(pass_context = True)
-async def inv(ctx):
+async def invite(ctx):
     inv = discord.Embed(title = "", color= 0x3079ec )
-    inv.set_author(name="Click here to invite", url = "https://discordapp.com/oauth2/authorize?client_id=505040895200985089&scope=bot&permissions=37088334")
-    inv.set_footer(text=Bot.user.name, icon_url = Bot.user.avatar_url)
+    inv.set_author(
+        name = "Click here to invite", 
+        url = "https://discordapp.com/oauth2/authorize?client_id=505040895200985089&scope=bot&permissions=37088334")
+    inv.set_footer(
+        text = Bot.user.name + " BOT", 
+        icon_url = Bot.user.avatar_url)
+
     main = discord.Embed(title= ":mailbox_with_mail: Sended! check pm", color= 0x39d0d6 )
     main.set_footer(text=f'Requested by: {ctx.message.author.name}')
+
     await ctx.send(embed=main)
     await ctx.message.author.send(embed=inv)
     await ctx.message.delete()
