eaec442f80c90f028a924433bad5e68750f9fdae
Nockech <49099272+Nockech@users.noreply.github.com>
2020-10-07 20:31:24 +0300

    Add files via upload

--- cogs/music.py
+++ cogs/music.py
@@ -1,65 +1,53 @@
 from discord.ext import commands
-import lavalink
-from discord import utils
-from discord import Embed
+import wavelink
+import discord
 
 class MusicCog(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-    self.bot.music = lavalink.Client(self.bot.user.id)
-    self.bot.music.add_node('localhost', 7000, 'testing', 'na', 'music-node')
-    self.bot.add_listener(self.bot.music.voice_update_handler, 'on_socket_response')
-    self.bot.music.add_event_hook(self.track_hook)
 
-  @commands.command(name='join')
-  async def join(self, ctx):
-    print('join command worked')
-    member = utils.find(lambda m: m.id == ctx.author.id, ctx.guild.members)
-    if member is not None and member.voice is not None:
-      vc = member.voice.channel
-      player = self.bot.music.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))
-      if not player.is_connected:
-        player.store('channel', ctx.channel.id)
-        await self.connect_to(ctx.guild.id, str(vc.id))
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command(name='play')
-  async def play(self, ctx, *, query):
-    try:
-      player = self.bot.music.player_manager.get(ctx.guild.id)
-      query = f'ytsearch:{query}'
-      results = await player.node.get_tracks(query)
-      tracks = results['tracks'][0:10]
-      i = 0
-      query_result = ''
-      for track in tracks:
-        i = i + 1
-        query_result = query_result + f'{i}) {track["info"]["title"]} - {track["info"]["uri"]}\n'
-      embed = Embed()
-      embed.description = query_result
+        if not hasattr(bot, 'wavelink'):
+            self.bot.wavelink = wavelink.Client(bot=self.bot)
 
-      await ctx.channel.send(embed=embed)
+        self.bot.loop.create_task(self.start_nodes())
 
-      def check(m):
-        return m.author.id == ctx.author.id
-      
-      response = await self.bot.wait_for('message', check=check)
-      track = tracks[int(response.content)-1]
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
 
-      player.add(requester=ctx.author.id, track=track)
-      if not player.is_playing:
-        await player.play()
+        await self.bot.wavelink.initiate_node(host='localhost',
+                                              port=2333,
+                                              rest_uri='http://localhost:2333',
+                                              password='youshallnotpass',
+                                              identifier='TEST',
+                                              region='us_central')
 
-    except Exception as error:
-      print(error)
-  
-  async def track_hook(self, event):
-    if isinstance(event, lavalink.events.QueueEndEvent):
-      guild_id = int(event.player.guild_id)
-      await self.connect_to(guild_id, None)
-      
-  async def connect_to(self, guild_id: int, channel_id: str):
-    ws = self.bot._connection._get_websocket(guild_id)
-    await ws.voice_state(str(guild_id), channel_id)
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel = None):
+        if not channel:
+            try:
+                channel = ctx.author.voice.channel
+            except AttributeError:
+                raise discord.DiscordException('No channel to join. Please either specify a valid channel or join one.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        await ctx.send(f'Connecting to **`{channel.name}`**')
+        await player.connect(channel.id)
+
+    @commands.command()
+    async def play(self, ctx, *, query: str):
+        tracks = await self.bot.wavelink.get_tracks(f'ytsearch:{query}')
+
+        if not tracks:
+            return await ctx.send('Could not find any songs with that query.')
+
+        player = self.bot.wavelink.get_player(ctx.guild.id)
+        if not player.is_connected:
+            await ctx.invoke(self.connect_)
+
+        await ctx.send(f'Added {str(tracks[0])} to the queue.')
+        await player.play(tracks[0])
+
 
 def setup(bot):
-  bot.add_cog(MusicCog(bot))
+    bot.add_cog(MusicCog(bot))
